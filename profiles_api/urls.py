2852c87a26c1cb9ab4bde3f6a01eaeebff2f9243
parag <parag.dicholkar@gmail.com>
2023-09-16 18:49:28 +0530

    View Sets view

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from profiles_api import views
 
 
 urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
     path('hello-view/', views.HelloApiView.as_view()),
 ]
